404038e10badead4d584b0ecba739b88d0beccc9
Raghavendra <121816454+jrragh-003@users.noreply.github.com>
2025-04-02 11:41:30 +0530

    inspect containers : to be reviewed

--- CVC/routes/container_routes.go
+++ CVC/routes/container_routes.go
@@ -12,7 +12,7 @@ func RegisterContainerRoutes(router *gin.Engine) {
 		containerGroup.POST("/createContainer/:imageName")
 		containerGroup.POST("/startContainer/:id", controllers.StartContainer)     // Start a new container
 		containerGroup.POST("/stopContainer/:id", controllers.StopContainer)       // Stop a container
-		containerGroup.GET("/inspectContainer/:id", controllers.InspectContainer)  // Inspect a container
+		//containerGroup.GET("/inspectContainer/:id", controllers.InspectContainer)  // Inspect a container
 		containerGroup.DELETE("/deleteContainer/:id", controllers.DeleteContainer) // Delete a container
 		containerGroup.GET("/getContainerDetails/:id", controllers.GetContainerDetails)
 	}
