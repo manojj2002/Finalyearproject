9752c452a6c91809342f223412b606088d263c5d
Manoj Janasale <manojjanasale@Manojs-MacBook-Air.local>
2025-05-06 13:29:25 +0530

    Added env file

--- CVC_BACKEND/config/db.go
+++ CVC_BACKEND/config/db.go
@@ -3,8 +3,10 @@ package config
 import (
 	"context"
 	"log"
+	"os"
 	"time"
 
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -13,8 +15,20 @@ var db *mongo.Database
 
 // InitDB initializes the MongoDB connection
 func InitDB() {
-	// Define MongoDB URI
-	clientOptions := options.Client().ApplyURI("")
+
+	// Load .env file
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("❌ Error loading .env file:", err)
+	}
+
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("❌ MONGODB_URI not set in environment")
+	}
+
+	clientOptions := options.Client().ApplyURI(uri)
 
 	// Set a timeout for connection
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
